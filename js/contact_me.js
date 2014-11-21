14953df70d532a63d043c541b28fac856c1b3b53
Rafael Fukuda <rafael.fukuda@gmail.com>
2014-11-21 18:59:27 -0200

    Contact Form

--- js/contact_me.js
+++ js/contact_me.js
@@ -8,49 +8,51 @@ $(function() {
         submitSuccess: function($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
+            var name = $("input#name").val(),
+                email = $("input#email").val(),
+                message = $("textarea#message").val();
+
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
+
+            var button = $("#send-btn").attr("disabled", "disabled");
+
             $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
+                url: "http://getsimpleform.com/messages/ajax?form_api_token=78df0a2763e793a7da433aa26bd8a765",
+                dataType: 'jsonp',
                 data: {
                     name: name,
-                    phone: phone,
                     email: email,
                     message: message
                 },
-                cache: false,
-                success: function() {
+                cache: false
+            }).done (function(result, status) {
+                if (result.success) {
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Sua mensagem foi enviada :) Obrigado! </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
-
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
+                    $('#contactForm').trigger("reset");                        
+                } else {
                     // Fail message
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>" + firstName + ", algo deu errado. Poderia tentar novamente?");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
-                },
-            })
+                }
+
+                button.removeAttr("disabled");
+            });
         },
         filter: function() {
             return $(this).is(":visible");
